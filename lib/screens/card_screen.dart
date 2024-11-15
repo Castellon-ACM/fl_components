8c218aaa0b404387bcb8e909b86240d6de3751f5
Alejandro Castellon <alexcastellon077@gmail.com>
2024-11-15 09:48:17 +0100

    Creamos 4 imagenes con titulo y tanto la URL como el titulo lo pasamos por parametro

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -16,7 +16,16 @@ class CardScreen extends StatelessWidget {
         children: const [
           CustomCardTipo1(),
           SizedBox(height: 10,),
-          CustomCardTipo2(),
+          CustomCardTipo2(imageUrl: 'https://wallpapers.com/images/featured/juego-de-tronos-92acb30ilmkjbmu9.jpg',titulo: 'Jhon Nieve',),
+          SizedBox(height: 10,),
+          CustomCardTipo2(imageUrl: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2017/07/juego-tronos-caminantes-blancos.jpg',titulo: 'Caminante Blanco',),
+          SizedBox(height: 10,),
+          CustomCardTipo2(imageUrl: 'https://cdn.avpasion.com/wp-content/uploads/2021/08/juego-de-tronos-hbo-max.jpg',titulo: 'Dragón',),
+          SizedBox(height: 10,),
+          CustomCardTipo2(imageUrl: 'https://wallpapers.com/images/hd/tv-4k-game-of-thrones-daenerys-targaryen-wqihe6int4ndmw6b.jpg',titulo: 'Daenerys Targaryen',),
+          SizedBox(height: 10,),
+          CustomCardTipo2(imageUrl: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/04/maisie-williams-como-arya-stark-juego-tronos-2674213.jpg',titulo: 'Arya Stark',),
+          SizedBox(height: 10,),
         ],
       )
       );
--- lib/widgets/customCardTipo2.dart
+++ lib/widgets/customCardTipo2.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class CustomCardTipo2 extends StatelessWidget {
-  const CustomCardTipo2({super.key});
-
+  final String imageUrl;
+  final String? titulo;
+  const CustomCardTipo2({super.key, required this.imageUrl, this.titulo});
+  
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -12,19 +14,19 @@ class CustomCardTipo2 extends StatelessWidget {
       ),
       child: Column(
         children: [
-          const FadeInImage(
-            image: NetworkImage('https://wallpapers.com/images/featured/juego-de-tronos-92acb30ilmkjbmu9.jpg'), 
-            placeholder: AssetImage('assets/jar-loading.gif'),
+            FadeInImage(
+            image: NetworkImage(imageUrl),
+            placeholder: const AssetImage('assets/jar-loading.gif'),
             width: double.infinity,
             //height: 260,
             //fit: BoxFit.cover,
-            fadeInDuration: Duration(milliseconds: 2000),
+            fadeInDuration: const Duration(milliseconds: 2000),
             ),
             Container(
               alignment: AlignmentDirectional.centerEnd,
             padding: const EdgeInsets.only( top: 10, bottom: 10, right: 20),
             child: 
-            const Text('Jhon Nieve'))
+            Text(titulo ?? 'Desconocido'))
         ],
       ),
     );
